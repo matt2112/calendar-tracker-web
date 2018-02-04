d032b0ff7efce2f2dfef7e8c56ed993d37fff1d7
Matt <matt_lewis@live.co.uk>
2018-02-04 16:51:35 +0000

    add options component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+
 import "./App.css";
+import Options from "./components/Options";
 
 class App extends Component {
+  state = {
+    maxDays: 4,
+    timePeriod: 5
+  };
+
+  changeOptionValue = (name, value) => {
+    console.log(value);
+    const parsedVal = value === "" ? 0 : parseInt(value, 10);
+    this.setState(() => ({
+      [name]: parsedVal
+    }));
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Calendar Tracker</h1>
+        <Options
+          maxDays={this.state.maxDays}
+          timePeriod={this.state.timePeriod}
+          onOptionChange={this.changeOptionValue}
+        />
       </div>
     );
   }
---
+++ src/components/Options.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+const Options = props => {
+  return (
+    <div className="options">
+      <h2 className="sub-heading">Options</h2>
+      <div className="option">
+        <p>Time Period</p>
+        <input
+          value={props.timePeriod}
+          onChange={e => props.onOptionChange("timePeriod", e.target.value)}
+        />
+      </div>
+      <div className="option">
+        <p>Maximum Days out of Country</p>
+        <input
+          value={props.maxDays}
+          onChange={e => props.onOptionChange("maxDays", e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+Options.propTypes = {
+  maxDays: PropTypes.number.isRequired,
+  onOptionChange: PropTypes.func.isRequired,
+  timePeriod: PropTypes.number.isRequired
+};
+
+export default Options;
